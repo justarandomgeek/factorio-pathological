7ac74d8727d1f73127574c82a17cb2c87f3f50c1
Thomas Deal <justarandomgeek@gmail.com>
2018-05-11 10:17:11 -0400

    Added Train With No Path Penalty for Factorio 0.16.42

--- data.lua
+++ data.lua
@@ -1,4 +1,8 @@
 for k,v in pairs(data.raw["utility-constants"]["default"].train_path_finding) do
-  data.raw["utility-constants"]["default"].train_path_finding[k] =
-    settings.startup["pathological-"..k].value
+  log("Pathologial loading " .. k)
+  if settings.startup["pathological-"..k] then
+    data.raw["utility-constants"]["default"].train_path_finding[k] =
+      settings.startup["pathological-"..k].value
+      log("ok")
+  end
 end
