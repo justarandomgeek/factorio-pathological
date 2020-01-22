37916d8b3999866a8e51b741a2c4b9315bebfc6a
Thomas Deal <justarandomgeek@gmail.com>
2020-01-21 19:28:14 -0500

    remove log spam

--- data.lua
+++ data.lua
@@ -1,8 +1,6 @@
 for k,v in pairs(data.raw["utility-constants"]["default"].train_path_finding) do
-  log("Pathologial loading " .. k)
   if settings.startup["pathological-"..k] then
     data.raw["utility-constants"]["default"].train_path_finding[k] =
       settings.startup["pathological-"..k].value
-      log("ok")
   end
 end
